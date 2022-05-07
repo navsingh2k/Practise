83fb16822b7b64a18722d02da00370db1b33ffd1
navsingh2k <snavneet795@gmail.com>
2022-05-07 09:58:55 +0530

    pair sum

--- Arrays/pai_sum.cpp
+++ Arrays/pai_sum.cpp
@@ -1,9 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-     int arr[] = {1, 5, 7, -1, 5};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    sort(arr,arr+n-1);
-    
-
+     int nums[] = {1, 5, 7, -4, 5};
+    int n = sizeof(nums)/sizeof(nums[0]);
+    sort(nums,nums+n);
+    int target = 3;
+    int l=0,r=n-1;
+    while(l<r)
+    {
+         if(nums[l]+nums[r] == target)
+            {
+               cout<<nums[l]<<","<<nums[r];
+               return 0;
+            }
+          else if(nums[l]+nums[r] > target){
+               r--;
+           }
+            else{
+                l++;
+            }
+        
+    }
+    cout<<"Pair not exists ";
 }
