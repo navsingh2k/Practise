6f526e6a516e5d05cf75b8c063f42e27c6e7f4c1
navsingh2k <snavneet795@gmail.com>
2022-06-17 17:36:52 +0530

    another method

--- Recursion/powerset_string.cpp
+++ Recursion/powerset_string.cpp
@@ -7,16 +7,17 @@ void powerset(string input,string output,int index)
         cout<<output<<endl;
         return;
     }
-    //EXCLUDE
-    powerset(input,output,index+1);
     //INCLUDE
     char ch = input[index];
     output+=ch;
     powerset(input,output,index+1);
+    //EXCLUDE
+    output.pop_back();
+    powerset(input,output,index+1);
 }
 int main()
 {
-    string s="abc";
+    string s="ab";
     string output="";
     int index = 0;
     powerset(s,output,index);
